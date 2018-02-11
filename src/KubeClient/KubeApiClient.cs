3988252b1e1d931bec13bb881c9d3fcd8618d83e
Adam Friedman <tintoy@tintoy.io>
2018-02-11 16:15:37 +1100

    Add support for client certificate authentication.

--- src/KubeClient/KubeApiClient.cs
+++ src/KubeClient/KubeApiClient.cs
@@ -111,7 +111,7 @@ namespace KubeClient
         }
 
         /// <summary>
-        ///     Create a new <see cref="KubeApiClient"/>.
+        ///     Create a new <see cref="KubeApiClient"/> using a bearer token for authentication.
         /// </summary>
         /// <param name="endPointUri">
         ///     The base address for the Kubernetes API end-point.
@@ -119,10 +119,13 @@ namespace KubeClient
         /// <param name="accessToken">
         ///     The access token to use for authentication to the API.
         /// </param>
+        /// <param name="expectServerCertificate">
+        ///     An optional server certificate to expect.
+        /// <param>
         /// <returns>
         ///     The configured <see cref="KubeApiClient"/>.
         /// </returns>
-        public static KubeApiClient Create(Uri endPointUri, string accessToken)
+        public static KubeApiClient Create(Uri endPointUri, string accessToken, X509Certificate2 expectServerCertificate = null)
         {
             if (endPointUri == null)
                 throw new ArgumentNullException(nameof(endPointUri));
@@ -130,8 +133,12 @@ namespace KubeClient
             if (String.IsNullOrWhiteSpace(accessToken))
                 throw new ArgumentException("Argument cannot be null, empty, or entirely composed of whitespace: 'accessToken'.", nameof(accessToken));
 
+            HttpClientHandler clientHandler = new HttpClientHandler();
+            if (expectServerCertificate != null)
+                clientHandler.ServerCertificateCustomValidationCallback = ServerCertificateValidator(expectServerCertificate);
+
             return new KubeApiClient(
-                new HttpClient
+                new HttpClient(clientHandler)
                 {
                     BaseAddress = endPointUri,
                     DefaultRequestHeaders =
@@ -141,6 +148,45 @@ namespace KubeClient
                             parameter: accessToken
                         )
                     }
+                }
+            );
+        }
+
+        /// <summary>
+        ///     Create a new <see cref="KubeApiClient"/> using an X.509 certificate for client authentication.
+        /// </summary>
+        /// <param name="endPointUri">
+        ///     The base address for the Kubernetes API end-point.
+        /// </param>
+        /// <param name="clientCertificate">
+        ///     The X.509 certificate to use for client authentication.
+        /// </param>
+        /// <param name="expectServerCertificate">
+        ///     An optional server certificate to expect.
+        /// <param>
+        /// <returns>
+        ///     The configured <see cref="KubeApiClient"/>.
+        /// </returns>
+        public static KubeApiClient Create(Uri endPointUri, X509Certificate2 clientCertificate, X509Certificate2 expectServerCertificate = null)
+        {
+            if (endPointUri == null)
+                throw new ArgumentNullException(nameof(endPointUri));
+            
+            if (clientCertificate == null)
+                throw new ArgumentNullException(nameof(clientCertificate));
+
+            HttpClientHandler clientHandler = new HttpClientHandler
+            {
+                ClientCertificates = { clientCertificate }
+            };
+
+            if (expectServerCertificate != null)
+                clientHandler.ServerCertificateCustomValidationCallback = ServerCertificateValidator(expectServerCertificate);
+
+            return new KubeApiClient(
+                new HttpClient(clientHandler)
+                {
+                    BaseAddress = endPointUri
                 }
             );
         }
@@ -160,49 +206,51 @@ namespace KubeClient
             if (String.IsNullOrWhiteSpace(kubeServiceHost))
                 throw new InvalidOperationException("KubeApiClient.CreateFromPodServiceAccount can only be called when running in a Kubernetes Pod (KUBERNETES_SERVICE_HOST environment variable is not defined).");
 
+            var baseAddress = new Uri($"https://kubernetes/");            
+            string authToken = File.ReadAllText("/var/run/secrets/kubernetes.io/serviceaccount/token");
             var kubeCACertificate = new X509Certificate2(
                 File.ReadAllBytes("/var/run/secrets/kubernetes.io/serviceaccount/ca.crt")
             );
 
-            HttpClientHandler clientHandler = new HttpClientHandler
+            return Create(baseAddress, authToken, expectServerCertificate: kubeCACertificate);
+        }
+
+        /// <summary>
+        ///     Create a certificate validation handler.
+        /// </summary>
+        /// <param name="expectCertificate">
+        ///     A specific certificate to expect.
+        /// </param>
+        /// <returns>
+        ///     The handler function.
+        /// </returns>
+        static Func<HttpRequestMessage, X509Certificate2, X509Chain, SslPolicyErrors, bool> ServerCertificateValidator(X509Certificate2 expectCertificate)
+        {
+            if (expectCertificate == null)
+                throw new ArgumentNullException(nameof(expectCertificate));
+            
+            return (request, certificate, chain, sslPolicyErrors) =>
             {
-                ServerCertificateCustomValidationCallback = (request, certificate, chain, sslPolicyErrors) =>
+                if (sslPolicyErrors != SslPolicyErrors.RemoteCertificateChainErrors)
+                    return false;
+
+                try
                 {
-                    if (sslPolicyErrors != SslPolicyErrors.RemoteCertificateChainErrors)
-                        return false;
+                    X509Chain kubeChain = new X509Chain();
+                    kubeChain.ChainPolicy.ExtraStore.Add(expectCertificate);
+                    kubeChain.ChainPolicy.VerificationFlags = X509VerificationFlags.AllowUnknownCertificateAuthority;
+                    kubeChain.ChainPolicy.RevocationMode = X509RevocationMode.NoCheck;
+                    
+                    return kubeChain.Build(certificate);
+                }
+                catch (Exception chainException)
+                {
+                    Debug.WriteLine(chainException);
+                    Console.WriteLine(chainException);
 
-                    try
-                    {
-                        X509Chain kubeChain = new X509Chain();
-                        kubeChain.ChainPolicy.ExtraStore.Add(kubeCACertificate);
-                        kubeChain.ChainPolicy.VerificationFlags = X509VerificationFlags.AllowUnknownCertificateAuthority;
-                        kubeChain.ChainPolicy.RevocationMode = X509RevocationMode.NoCheck;
-                        
-                        return kubeChain.Build(certificate);
-                    }
-                    catch (Exception chainException)
-                    {
-                        Debug.WriteLine(chainException);
-                        Console.WriteLine(chainException);
-
-                        return false;
-                    }
+                    return false;
                 }
             };
-
-            return new KubeApiClient(
-                new HttpClient(clientHandler)
-                {
-                    BaseAddress = new Uri($"https://kubernetes/"),
-                    DefaultRequestHeaders =
-                    {
-                        Authorization = new AuthenticationHeaderValue(
-                            scheme: "Bearer",
-                            parameter: File.ReadAllText("/var/run/secrets/kubernetes.io/serviceaccount/token")
-                        )
-                    }
-                }
-            );
         }
     }
 }
