d8885fd9d91e6e6aa3428abbf20f4f975e2b5afc
Adam Friedman <tintoy@tintoy.io>
2018-02-14 12:41:57 +1100

    Improve build script.

--- build.ps1
+++ build.ps1
@@ -1,7 +1,9 @@
+$ErrorActionPreference = 'Stop'
+
 $versionInfo = .\tools\GitVersion\GitVersion.exe | ConvertFrom-Json
 Write-Host $versionInfo
 
 $dotnet = Get-Command dotnet
-& $dotnet restore "/p:VersionPrefix=$versionInfo.MajorMinorPatch" "/p:VersionSuffix=$versionInfo.PreReleaseTag" "/p:AssemblyInformationalVersion=$versionInfo.InformationalVersion"
-& $dotnet build "/p:VersionPrefix=$versionInfo.MajorMinorPatch" "/p:VersionSuffix=$versionInfo.PreReleaseTag" "/p:AssemblyInformationalVersion=$versionInfo.InformationalVersion"
-& $dotnet pack "/p:VersionPrefix=$versionInfo.MajorMinorPatch" "/p:VersionSuffix=$versionInfo.PreReleaseTag" "/p:AssemblyInformationalVersion=$versionInfo.InformationalVersion" -o $PWD\out
+& $dotnet restore "/p:VersionPrefix=$versionInfo.MajorMinorPatch" "/p:VersionSuffix=$versionInfo.PreReleaseTag" "/p:AssemblyInformationalVersion=$versionInfo.InformationalVersion" /v:n
+& $dotnet build "/p:VersionPrefix=$versionInfo.MajorMinorPatch" "/p:VersionSuffix=$versionInfo.PreReleaseTag" "/p:AssemblyInformationalVersion=$versionInfo.InformationalVersion" /v:n
+& $dotnet pack "/p:VersionPrefix=$versionInfo.MajorMinorPatch" "/p:VersionSuffix=$versionInfo.PreReleaseTag" "/p:AssemblyInformationalVersion=$versionInfo.InformationalVersion" /v:n -o $PWD\out
