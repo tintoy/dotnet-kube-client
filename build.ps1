1f4ea9f04f1e3176a779ee1bbc73c47bcd86e985
Adam Friedman <tintoy@tintoy.io>
2018-02-14 12:43:35 +1100

    Build script diagnostics.

--- build.ps1
+++ build.ps1
@@ -3,7 +3,11 @@ $ErrorActionPreference = 'Stop'
 $versionInfo = .\tools\GitVersion\GitVersion.exe | ConvertFrom-Json
 Write-Host $versionInfo
 
+Dir -Recurse $PWD
+
 $dotnet = Get-Command dotnet
-& $dotnet restore "/p:VersionPrefix=$versionInfo.MajorMinorPatch" "/p:VersionSuffix=$versionInfo.PreReleaseTag" "/p:AssemblyInformationalVersion=$versionInfo.InformationalVersion" /v:n
-& $dotnet build "/p:VersionPrefix=$versionInfo.MajorMinorPatch" "/p:VersionSuffix=$versionInfo.PreReleaseTag" "/p:AssemblyInformationalVersion=$versionInfo.InformationalVersion" /v:n
-& $dotnet pack "/p:VersionPrefix=$versionInfo.MajorMinorPatch" "/p:VersionSuffix=$versionInfo.PreReleaseTag" "/p:AssemblyInformationalVersion=$versionInfo.InformationalVersion" /v:n -o $PWD\out
+& $dotnet restore "/p:VersionPrefix=$versionInfo.MajorMinorPatch" "/p:VersionSuffix=$versionInfo.PreReleaseTag" "/p:AssemblyInformationalVersion=$versionInfo.InformationalVersion" /v:d
+& $dotnet build "/p:VersionPrefix=$versionInfo.MajorMinorPatch" "/p:VersionSuffix=$versionInfo.PreReleaseTag" "/p:AssemblyInformationalVersion=$versionInfo.InformationalVersion" /v:d
+& $dotnet pack "/p:VersionPrefix=$versionInfo.MajorMinorPatch" "/p:VersionSuffix=$versionInfo.PreReleaseTag" "/p:AssemblyInformationalVersion=$versionInfo.InformationalVersion" /v:d -o $PWD\out
+
+Dir -Recurse $PWD
