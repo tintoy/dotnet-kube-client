3bf8055e81311c5ff156229e6cf150b7bb045bad
Adam Friedman <tintoy@tintoy.io>
2018-02-23 21:13:21 +1100

    Use correct terminology for KubeResourceAttribute.

--- src/KubeClient/Models/KubeResourceAttribute.cs
+++ src/KubeClient/Models/KubeResourceAttribute.cs
@@ -12,28 +12,28 @@ namespace KubeClient.Models
         /// <summary>
         ///     Mark the model class as representing a Kubernetes resource.
         /// </summary>
-        /// <param name="name">
-        ///     The resource name.
+        /// <param name="kind">
+        ///     The resource kind.
         /// </param>
         /// <param name="version">
         ///     The resource API version.
         /// </param>
-        public KubeResourceAttribute(string name, string version)
+        public KubeResourceAttribute(string kind, string version)
         {
-            if (String.IsNullOrWhiteSpace(name))
-                throw new ArgumentException("Argument cannot be null, empty, or entirely composed of whitespace: 'name'.", nameof(name));
+            if (String.IsNullOrWhiteSpace(kind))
+                throw new ArgumentException("Argument cannot be null, empty, or entirely composed of whitespace: 'name'.", nameof(kind));
 
             if (String.IsNullOrWhiteSpace(version))
                 throw new ArgumentException("Argument cannot be null, empty, or entirely composed of whitespace: 'version'.", nameof(version));
 
-            Name = name;
+            Kind = kind;
             Version = version;
         }
 
         /// <summary>
-        ///     The resource name.
+        ///     The resource kind.
         /// </summary>
-        public string Name { get; }
+        public string Kind { get; }
 
         /// <summary>
         ///     The resource API version.
