5e7005187e1f30afb63cb0d0ec0e15320214c7ba
Adam Friedman <tintoy@tintoy.io>
2018-02-26 10:56:37 +1100

    Fix auto-population of KubeObjectV1 Kind and ApiVersion.

---
+++ test/KubeClient.Tests/KubeObjectTests.cs
@@ -0,0 +1,26 @@
+using System;
+using Xunit;
+
+namespace KubeClient.Tests
+{
+    using Models;
+
+    /// <summary>
+    ///     Tests for the <see cref="KubeObjectV1"/> base class.
+    /// </summary>
+    public class KubeObjectTests
+    {
+        /// <summary>
+        ///     Verify that KubeObjectV1 correctly populates Kind and ApiVersion when constructed.
+        /// </summary>
+        [InlineData(typeof(DeleteOptionsV1), "DeleteOptions", "v1")]
+        [InlineData(typeof(DeploymentV1Beta1), "Deployment", "v1beta1")]
+        [Theory(DisplayName = "KubeObjectV1 correctly populates Kind and ApiVersion ")]
+        public void KubeObjectV1_Kind_Success(Type kubeObjectType, string expectedKind, string expectedApiVersion)
+        {
+            KubeObjectV1 kubeObject = (KubeObjectV1)Activator.CreateInstance(kubeObjectType);
+            Assert.Equal(expectedKind, kubeObject.Kind);
+            Assert.Equal(expectedApiVersion, kubeObject.ApiVersion);
+        }
+    }
+}
