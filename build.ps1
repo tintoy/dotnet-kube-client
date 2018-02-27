d79ca9fbc919abe5159a382aaf955a2681d410a8
Adam Friedman <tintoy@tintoy.io>
2018-02-28 06:52:07 +1100

    Print build version at the start of each CI build.

--- build.ps1
+++ build.ps1
@@ -6,6 +6,12 @@ $versionPrefix = $versionInfo.MajorMinorPatch
 $versionSuffix = $versionInfo.NuGetPreReleaseTagV2
 $informationalVersion = $versionInfo.InformationalVersion
 
+If ($versionSuffix) {
+    Write-Host "Build version is $versionPrefix-$versionSuffix"
+} Else {
+    Write-Host "Build version is $versionPrefix"
+}
+
 $dotnet = Get-Command dotnet
 & $dotnet restore /p:VersionPrefix="$versionPrefix" /p:VersionSuffix="$versionSuffix" /p:AssemblyInformationalVersion="$informationalVersion"
 & $dotnet build /p:VersionPrefix="$versionPrefix" /p:VersionSuffix="$versionSuffix" /p:AssemblyInformationalVersion="$informationalVersion"
