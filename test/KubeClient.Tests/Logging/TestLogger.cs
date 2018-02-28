52341b1e6bb973d970f5d1d97409bcdce4eb6210
Adam Friedman <tintoy@tintoy.io>
2018-03-01 07:48:16 +1100

    Add tests for KubeApiClient logging.

---
+++ test/KubeClient.Tests/Logging/TestLogger.cs
@@ -0,0 +1,107 @@
+using System;
+using System.Collections.Immutable;
+using System.Reactive.Disposables;
+using System.Reactive.Subjects;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Internal;
+
+namespace KubeClient.Tests.Logging
+{
+    /// <summary>
+    /// 	A stub implementation of <see cref="ILogger"/> for use in tests.
+    /// </summary>
+    public sealed class TestLogger
+		: ILogger, IDisposable
+	{
+        /// <summary>
+        ///     The subject used to publish log entries.
+        /// </summary>
+		readonly Subject<LogEntry> _logEntries = new Subject<LogEntry>();
+
+		/// <summary>
+		/// 	Create a new <see cref="TestLogger"/>.
+		/// </summary>
+		/// <param name="logLevel">
+		/// 	The logger's minimum log level.
+		/// </param>
+		public TestLogger(LogLevel logLevel)
+		{
+			LogLevel = logLevel;
+		}
+
+		/// <summary>
+		///		Dispose of resources being used by the <see cref="TestLogger"/>.
+		/// </summary>
+		public void Dispose() => _logEntries.Dispose();
+
+		/// <summary>
+		/// 	The logger's minimum log level.
+		/// </summary>
+		public LogLevel LogLevel { get; }
+
+		/// <summary>
+		///		An observable sequence of log entries emitted by the <see cref="TestLogger"/>.
+		/// </summary>
+		public IObservable<LogEntry> LogEntries => _logEntries;
+
+		/// <summary>
+        /// 	Emit a log entry.
+        /// </summary>
+        /// <param name="level">
+		/// 	The log entry's level.
+		/// </param>
+        /// <param name="eventId">
+		/// 	The log entry's associated event Id.
+		/// </param>
+        /// <param name="state">
+		/// 	The log entry to be written. Can be also an object.
+		/// </param>
+        /// <param name="exception">
+		/// 	The exception (if any) related to the log entry.
+		/// </param>
+        /// <param name="formatter">
+		/// 	A function that creates a <c>string</c> log message from the <paramref name="state"/> and <paramref name="exception"/>.
+		/// </param>
+        public void Log<TState>(LogLevel level, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
+		{
+			if (formatter == null)
+				throw new ArgumentNullException(nameof(formatter));
+
+			ImmutableDictionary<string, object> properties =
+				(state is FormattedLogValues formattedLogValues)
+					? ImmutableDictionary.CreateRange(formattedLogValues)
+					: ImmutableDictionary<string, object>.Empty;
+
+			_logEntries.OnNext(new LogEntry(
+				level,
+				eventId,
+				formatter(state, exception),
+				exception,
+				state,
+				properties
+			));
+		}
+
+        /// <summary>
+        /// 	Check if the given <paramref name="logLevel"/> is enabled.
+        /// </summary>
+        /// <param name="logLevel">
+		/// 	The level to be checked.
+		/// </param>
+        /// <returns>
+		/// 	<c>true</c> if enabled; otherwise, <c>false</c>.
+		/// </returns>
+        public bool IsEnabled(LogLevel logLevel) => logLevel >= LogLevel;
+
+        /// <summary>
+        /// 	Begin a logical operation scope.
+        /// </summary>
+        /// <param name="state">
+		/// 	An identifier for the scope.
+		/// </param>
+        /// <returns>
+		/// 	An <see cref="IDisposable"/> that ends the logical operation scope when disposed.
+		/// </returns>
+		public IDisposable BeginScope<TState>(TState state) => Disposable.Empty;
+	}
+}
---
+++ test/KubeClient.Tests/LoggingTests.cs
@@ -0,0 +1,167 @@
+using HTTPlease;
+using HTTPlease.Diagnostics;
+using HTTPlease.Formatters;
+using HTTPlease.Testability;
+using HTTPlease.Testability.Mocks;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Xunit;
+using Newtonsoft.Json;
+
+using EventIds = HTTPlease.Diagnostics.MessageHandlers.LoggerExtensions.LogEventIds;
+
+namespace KubeClient.Tests
+{
+    using Logging;
+    using Models;
+
+    /// <summary>
+    ///     Tests for <see cref="KubeApiClient"/> logging.
+    /// </summary>
+    public class LoggingTests
+    {
+        /// <summary>
+        ///     Create a new <see cref="KubeApiClient"/> logging test suite.
+        /// </summary>
+        public LoggingTests()
+        {
+        }
+
+        /// <summary>
+        ///     Verify that the client's logger emits BeginRequest / EndRequest log entries for PodV1 get-by-name.
+        /// </summary>
+        [Fact(DisplayName = "Emit BeginRequest / EndRequest log entries for PodV1 get-by-name (NotFound)")]
+        public async Task PodsV1_GetByName_NotFound()
+        {
+            var logEntries = new List<LogEntry>();
+
+			TestLogger logger = new TestLogger(LogLevel.Information);
+			logger.LogEntries.Subscribe(
+				logEntry => logEntries.Add(logEntry)
+			);
+
+			ClientBuilder clientBuilder = new ClientBuilder()
+				.WithLogging(logger);
+
+			HttpClient httpClient = clientBuilder.CreateClient("http://localhost:1234", new MockMessageHandler(
+				request => request.CreateResponse(HttpStatusCode.NotFound,
+                    responseBody: JsonConvert.SerializeObject(new StatusV1
+                    {
+                        Reason = "NotFound"
+                    }),
+                    WellKnownMediaTypes.Json
+                )
+			));
+
+            using (KubeApiClient kubeClient = KubeApiClient.Create(httpClient))
+            {
+                PodV1 pod = await kubeClient.PodsV1().Get(name: "foo");
+                Assert.Null(pod);
+            }
+
+            Assert.Equal(2, logEntries.Count);
+
+			LogEntry logEntry1 = logEntries[0];
+			Assert.Equal(EventIds.BeginRequest, logEntry1.EventId.Id);
+			Assert.Equal("Performing GET request to 'http://localhost:1234/api/v1/namespaces/default/pods/foo'.",
+				logEntry1.Message
+			);
+			Assert.Equal("GET",
+				logEntry1.Properties["Method"]
+			);
+			Assert.Equal(new Uri("http://localhost:1234/api/v1/namespaces/default/pods/foo"),
+				logEntry1.Properties["RequestUri"]
+			);
+
+			LogEntry logEntry2 = logEntries[1];
+			Assert.Equal(EventIds.EndRequest, logEntry2.EventId.Id);
+			Assert.Equal("Completed GET request to 'http://localhost:1234/api/v1/namespaces/default/pods/foo' (NotFound).",
+				logEntry2.Message
+			);
+			Assert.Equal("GET",
+				logEntry2.Properties["Method"]
+			);
+			Assert.Equal(new Uri("http://localhost:1234/api/v1/namespaces/default/pods/foo"),
+				logEntry2.Properties["RequestUri"]
+			);
+			Assert.Equal(HttpStatusCode.NotFound,
+				logEntry2.Properties["StatusCode"]
+			);
+        }
+
+        /// <summary>
+        ///     Verify that the client's logger emits BeginRequest / EndRequest log entries for PodV1 get-by-name.
+        /// </summary>
+        [Fact(DisplayName = "Emit BeginRequest / EndRequest log entries for PodV1 get-by-name (OK)")]
+        public async Task PodsV1_GetByName_OK()
+        {
+            var logEntries = new List<LogEntry>();
+
+			TestLogger logger = new TestLogger(LogLevel.Information);
+			logger.LogEntries.Subscribe(
+				logEntry => logEntries.Add(logEntry)
+			);
+
+			ClientBuilder clientBuilder = new ClientBuilder()
+				.WithLogging(logger);
+
+			HttpClient httpClient = clientBuilder.CreateClient("http://localhost:1234", new MockMessageHandler(
+				request => request.CreateResponse(HttpStatusCode.OK,
+                    responseBody: JsonConvert.SerializeObject(new PodV1
+                    {
+                        Metadata = new ObjectMetaV1
+                        {
+                            Name = "foo",
+                            Namespace = "default"
+                        }
+                    }),
+                    WellKnownMediaTypes.Json
+                )
+			));
+
+            using (KubeApiClient kubeClient = KubeApiClient.Create(httpClient))
+            {
+                PodV1 pod = await kubeClient.PodsV1().Get(name: "foo");
+                Assert.NotNull(pod);
+                Assert.NotNull(pod.Metadata);
+                Assert.Equal("foo", pod.Metadata.Name);
+                Assert.Equal("default", pod.Metadata.Namespace);
+            }
+
+            Assert.Equal(2, logEntries.Count);
+
+			LogEntry logEntry1 = logEntries[0];
+            Assert.Equal(LogLevel.Debug, logEntry1.Level);
+			Assert.Equal(EventIds.BeginRequest, logEntry1.EventId.Id);
+			Assert.Equal("Performing GET request to 'http://localhost:1234/api/v1/namespaces/default/pods/foo'.",
+				logEntry1.Message
+			);
+			Assert.Equal("GET",
+				logEntry1.Properties["Method"]
+			);
+			Assert.Equal(new Uri("http://localhost:1234/api/v1/namespaces/default/pods/foo"),
+				logEntry1.Properties["RequestUri"]
+			);
+
+			LogEntry logEntry2 = logEntries[1];
+            Assert.Equal(LogLevel.Debug, logEntry2.Level);
+			Assert.Equal(EventIds.EndRequest, logEntry2.EventId.Id);
+			Assert.Equal("Completed GET request to 'http://localhost:1234/api/v1/namespaces/default/pods/foo' (OK).",
+				logEntry2.Message
+			);
+			Assert.Equal("GET",
+				logEntry2.Properties["Method"]
+			);
+			Assert.Equal(new Uri("http://localhost:1234/api/v1/namespaces/default/pods/foo"),
+				logEntry2.Properties["RequestUri"]
+			);
+			Assert.Equal(HttpStatusCode.OK,
+				logEntry2.Properties["StatusCode"]
+			);
+        }
+    }
+}
