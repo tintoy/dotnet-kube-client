9e5775f1bcb8c8ffdfeba169087bea32b3cd99d0
Adam Friedman <tintoy@tintoy.io>
2018-03-08 06:19:36 +1100

    Use Travis for CI builds on feature/websockets branch.
    Hopefully, Travis will install and the 2.1 preview of the .NET Core SDK (something AppVeyor doesn't handle yet).

---
+++ build-ci.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+set -e
+set -u
+set -o pipefail
+
+############################
+# Build script for Travis CI
+############################
+
+echo 'Computing build version...'
+
+mono $PWD/tools/GitVersion/GitVersion.exe | tee $PWD/version-info.json
+
+BUILD_BASEVERSION=$(cat $PWD/version-info.json | jq -r .MajorMinorPatch)
+BUILD_VERSION_SUFFIX=$(cat $PWD/version-info.json | jq -r .NuGetPreReleaseTagV2)
+BUILD_INFORMATIONAL_VERSION=$(cat $PWD/version-info.json | jq -r .InformationalVersion)
+
+echo ''
+if [ -z "$BUILD_VERSION_SUFFIX" ]; then
+	echo "Build version is '$BUILD_BASEVERSION'."
+else
+	echo "Build version is '$BUILD_BASEVERSION-$BUILD_VERSION_SUFFIX'."
+fi
+echo ''
+
+# Build outputs go here.
+ARTIFACTS_DIRECTORY="$PWD/artifacts"
+if [ -d $ARTIFACTS_DIRECTORY ]; then
+	rm -rf $ARTIFACTS_DIRECTORY
+fi
+
+echo ''
+echo 'Restoring packages...'
+echo ''
+
+dotnet restore /p:VersionPrefix="$BUILD_BASEVERSION" /p:VersionSuffix="$BUILD_VERSION_SUFFIX" /p:AssemblyInformationalVersion="$BUILD_INFORMATIONAL_VERSION"
+
+echo ''
+echo 'Building...'
+echo ''
+
+dotnet build /p:VersionPrefix="$BUILD_BASEVERSION" /p:VersionSuffix="$BUILD_VERSION_SUFFIX" /p:AssemblyInformationalVersion="$BUILD_INFORMATIONAL_VERSION" --no-restore
+
+echo ''
+echo 'Testing...'
+echo ''
+
+testProjects=$(find ./test -name 'KubeClient*.Tests.csproj')
+for testProject in $testProjects; do
+	dotnet test $testProject --no-build --no-restore
+done
+
+echo ''
+echo "Packing into '$ARTIFACTS_DIRECTORY'..."
+echo ''
+
+dotnet pack /p:VersionPrefix="$BUILD_BASEVERSION" /p:VersionSuffix="$BUILD_VERSION_SUFFIX" /p:AssemblyInformationalVersion="$BUILD_INFORMATIONAL_VERSION" -o $ARTIFACTS_DIRECTORY
