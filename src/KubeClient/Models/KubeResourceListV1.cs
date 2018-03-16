7cf485ee9feaabb70e854daf50a9cb4ef729fb46
Adam Friedman <tintoy@tintoy.io>
2018-03-17 07:20:27 +1100

    Add generic base class for resource list models.

--- src/KubeClient/Models/KubeResourceListV1.cs
+++ src/KubeClient/Models/KubeResourceListV1.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using System.Collections.Generic;
 using Newtonsoft.Json;
 
@@ -14,5 +15,36 @@ namespace KubeClient.Models
         /// </summary>
         [JsonProperty("metadata")]
         public ListMetaV1 Metadata { get; set; }
+    }
+
+    /// <summary>
+    ///     The base class for Kubernetes resource lists where the resource type is known.
+    /// </summary>
+    /// <typeparam name="TResource">
+    ///     The type of resource contained in the list.
+    /// </typeparam>
+    public abstract class KubeResourceListV1<TResource>
+        : KubeResourceListV1, IEnumerable<TResource>
+    {
+        /// <summary>
+        ///     The list's resources.
+        /// </summary>
+        public abstract List<TResource> Items { get; }
+
+        /// <summary>
+        ///     Get a typed enumerator for the list's resources.
+        /// </summary>
+        /// <returns>
+        ///     The <see cref="IEnumerator{T}"/>.
+        /// </returns>
+        public IEnumerator<TResource> GetEnumerator() => Items.GetEnumerator();
+
+        /// <summary>
+        ///     Get an untyped enumerator for the list's resources.
+        /// </summary>
+        /// <returns>
+        ///     The <see cref="IEnumerator"/>.
+        /// </returns>
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
     }
 }
